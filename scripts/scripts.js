1a31bd8b025ee3cc5105b521277b5254d5aeb8f4
raimieltan <raimieltan@gmail.com>
2020-10-25 12:42:56 +0800

    added questions with choices template

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -4,27 +4,47 @@ window.onload = function(){
 
     const questions = [
         {
-            question: "Labore culpa cupidatat nostrud ad.",
+            question: "Labore culpa cupidatat nostrud ad?",
             choices: ["a","b","c","d"]
         },
         {
-            question: "Voluptate ea occaecat occaecat dolor sunt adipisicing proident qui proident laborum.",
+            question: "Voluptate ea occaecat occaecat dolor sunt adipisicing proident qui proident laborum?",
             choices: ["a","b","c","d"]
         },
         {
-            question: "Ullamco sint duis pariatur sint consectetur aliqua ullamco sit ex amet sint consequat cillum consequat.",
+            question: "Ullamco sint duis pariatur sint consectetur aliqua ullamco sit ex amet sint consequat cillum consequat?",
             choices: ["a","b","c","d"]
         },
         {
-            question: "Voluptate consectetur consectetur consequat eu ipsum irure aliquip.",
+            question: "Voluptate consectetur consectetur consequat eu ipsum irure aliquip?",
             choices: ["a","b","c","d"]
         },
         {
-            question: "Incididunt quis irure aliqua in commodo aliqua magna proident commodo.",
+            question: "Incididunt quis irure aliqua in commodo aliqua magna proident commodo?",
             choices: ["a","b","c","d"]
         },
     ]
 
+    for(let i = 0; i < questions.length; i++){
+        let quizDiv = document.createElement('div');
+        let questId = document.createAttribute('id')
+        questId.value = `id${i}`
+        quizDiv.setAttributeNode(questId)
+
+        quizDiv.innerHTML = `
+        <h1 id='question${i}'>${questions[i].question}</h1>
+        `
+        document.querySelector('.quiz').appendChild(quizDiv)
+
+
+        for(let j = 0; j < questions[i].choices.length; j++){
+            let choice = document.createElement('button');
+            choice.innerHTML = `<button type='button'>${questions[i].choices[j]}</button>`
+            document.querySelector(`#id${i}`).appendChild(choice);
+        }
+
+        
+    }
     button.onclick = function handleClick(){
         let sidebar = document.querySelector('.sidebar');
         if (sidebar.style.display === "block") {
